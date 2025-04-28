e96b346dbca460450182c452148c525dc47eadfc
notmoonar <franciscogabriel0360@gmail.com>
2025-04-28 20:01:03 -0300

    refactor: remove MongoDB connection logic from testMongo.js

--- testMongo.js
+++ testMongo.js
@@ -3,14 +3,3 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const mongoURL = process.env.MONGODB_URL;
-
-mongoose
-  .connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("MongoDB connection successful!");
-    process.exit(0); // Exit the script
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-    process.exit(1); // Exit with an error
-  });
